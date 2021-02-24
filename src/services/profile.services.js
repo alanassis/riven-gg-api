8267aa47bdf2cf63df640224d459cfab59a7134b
Alan Assis <alanpenteado.work@gmail.com>
2021-02-24 19:33:48 -0300

    feat: retorna o id no getByNick

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -1,15 +1,19 @@
 const { LolApi } = require("twisted");
-const { getSummonerId } = require("./utils");
+const { throwError } = require("./utils");
 const lolApi = new LolApi(process.env.RIOT_KEY);
 
 module.exports = {
   async getByNick(region, nick) {
-    const summoner = await getSummonerId(nick, region);
+    if (nick.length < 3 || nick.length > 16)
+      throwError("Nome de invocador inválido", 400);
+
+    const summoner = (await lolApi.Summoner.getByName(nick, region)).response;
 
     const currentPatch = (await lolApi.DataDragon.getVersions())[0];
     const iconLink = `http://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/profileicon/${summoner.profileIconId}.png`;
 
     return {
+      id: summoner.id,
       nickname: summoner.name,
       level: summoner.summonerLevel,
       profileIcon: iconLink,
