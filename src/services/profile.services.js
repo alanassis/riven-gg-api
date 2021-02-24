e894d85cfa950f3427606a95f8733cb44224b4a8
Alan Assis <alanpenteado.work@gmail.com>
2021-02-24 19:22:05 -0300

    improve: melhora a função getByNick

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -1,23 +1,10 @@
-const { LolApi, Constants } = require("twisted");
-const { throwError } = require("./utils");
+const { LolApi } = require("twisted");
+const { getSummonerId } = require("./utils");
 const lolApi = new LolApi(process.env.RIOT_KEY);
 
 module.exports = {
   async getByNick(region, nick) {
-    if (nick.length < 3 || nick.length > 16)
-      throwError("Nome de invocador inválido", 400);
-
-    let selectedRegion;
-    for (let i in Constants.Regions) {
-      if (region.toLocaleLowerCase() === i.toLocaleLowerCase()) {
-        selectedRegion = i;
-      }
-    }
-
-    if (!selectedRegion) throwError("Região inválida", 400);
-
-    const summoner = (await lolApi.Summoner.getByName(nick, selectedRegion))
-      .response;
+    const summoner = await getSummonerId(nick, region);
 
     const currentPatch = (await lolApi.DataDragon.getVersions())[0];
     const iconLink = `http://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/profileicon/${summoner.profileIconId}.png`;
