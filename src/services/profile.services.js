6e20c267d9e6f932c22bcfee6c133ecd6c153a70
Alan Assis <alanpenteado.work@gmail.com>
2021-02-26 02:12:53 -0300

    feat: adiciona serviço de buscar histórico de partidas

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -1,4 +1,4 @@
-const { LolApi } = require("twisted");
+const { LolApi, Constants } = require("twisted");
 const { throwError } = require("./utils");
 const lolApi = new LolApi(process.env.RIOT_KEY);
 
@@ -13,7 +13,8 @@ module.exports = {
     const iconLink = `http://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/profileicon/${summoner.profileIconId}.png`;
 
     return {
-      id: summoner.id,
+      accountId: summoner.accountId,
+      summonerId: summoner.id,
       region,
       nickname: summoner.name,
       level: summoner.summonerLevel,
@@ -21,9 +22,60 @@ module.exports = {
     };
   },
 
-  async getLeagueByNick(region, summonerId) {
+  async getLeagues(region, summonerId) {
     const league = (await lolApi.League.bySummoner(summonerId, region))
       .response;
     return league;
+  },
+
+  async getMatches(region, accountId) {
+    const { response } = await lolApi.Match.list(accountId, region, {
+      champion: Constants.Champions.RIVEN,
+      endIndex: 10,
+    });
+
+    const matches = await Promise.all(
+      response.matches.map(async (m) => {
+        const { gameId, participants } = await this.getMatchDetail(
+          m.platformId,
+          m.gameId
+        );
+
+        const summonerParticipant = participants.find(
+          (par) => par.championId == m.champion
+        );
+
+        if (!summonerParticipant) throwError("Erro interno: 1");
+
+        const enemyParticipant = participants.find(
+          (par) =>
+            par.timeline.lane == summonerParticipant.timeline.lane &&
+            par.championId != m.champion
+        );
+
+        if (!enemyParticipant) throwError("Erro interno: 2");
+
+        summonerParticipant.championName = Constants.getChampionNameCapital(
+          summonerParticipant.championId
+        );
+
+        delete summonerParticipant.championId;
+
+        enemyParticipant.championName = Constants.getChampionNameCapital(
+          enemyParticipant.championId
+        );
+
+        delete enemyParticipant.championId;
+
+        return { gameId, summonerParticipant, enemyParticipant };
+      })
+    );
+
+    return matches;
+  },
+
+  async getMatchDetail(region, gameId) {
+    const data = await lolApi.Match.get(gameId, region);
+    return data.response;
   },
 };
